b21c340d4b93a6ebb39e62e19d91b2d178491dd8
Ethan Mines <elmines325@gmail.com>
2019-02-06 16:27:55 -0600

    Remove restriction on values held by Lexeme

--- Lexeme.java
+++ Lexeme.java
@@ -12,14 +12,10 @@ class Lexeme {
 	 */
 	public Lexeme(Type type, Lexeme l, Lexeme r, int lineNumber) {
 		this.type = type;
-		this.strVal = null;
-		this.intVal = null;
-		this.boolVal = null;
-
+		this.val = null;
 		this.l = l;
 		this.r = r;
 		this.lineNumber = lineNumber;
-
 	}
 	/*
 	 * Construc a nonterminal lexeme, with line number inferred from l and r
@@ -32,41 +28,14 @@ class Lexeme {
 		this(lineNumber, type, "");
 	}
 
-
-	public Lexeme(int lineNumber, Type type, String value) {
+	public Lexeme(int lineNumber, Type type, Object value) {
 		this.lineNumber = lineNumber;
 		this.type = type;
-
-		this.strVal = value;
-		this.intVal = null;
-		this.boolVal = null;
-		this.l = null;
-		this.r = null;
-
-	}
-
-	public Lexeme(int lineNumber, Type type, Integer value) {
-		this.lineNumber = lineNumber;
-		this.type = type;
-
-		this.strVal = null;
-		this.intVal = value;
-		this.boolVal = null;
-		this.l = null;
-		this.r = null;
-
-	}
-
-	public Lexeme(int lineNumber, Type type, Boolean value) {
-		this.lineNumber = lineNumber;
-		this.type = type;
-
-		this.strVal = null;
-		this.intVal = null;
-		this.boolVal = value;
+		this.val = value;
 		this.l = null;
 		this.r = null;
 	}
+
 
 	/**
 	 * @return A new Lexeme with all of orig's fields, except for its new Type
@@ -88,16 +57,12 @@ class Lexeme {
 		else                    return String.format("%d: %s", this.lineNumber, this.type.toString());
 	}
 
-	private final Boolean boolVal;
-	private final Integer intVal;
-	private final String strVal;
+	private final Object val;
 	private Lexeme l, r;
 
 	private String valueString() {
-		if (this.intVal != null) return this.intVal.toString();
-		else if (this.boolVal != null) return this.boolVal.toString();
-		else if (this.strVal != null) return this.strVal;
-		else                          return "";
+		if (this.val != null) return this.val.toString();
+		else                  return "";
 	}
 
 	private static int inferLineNumber(Lexeme l, Lexeme r) {
